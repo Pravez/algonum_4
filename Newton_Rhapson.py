540e72736b8a3dd7be9a8c872414c2574cdacb37
Pravez <paul.ravezb@gmail.com>
2016-04-01 11:07:50 +0200

    Suppression de sympy et de la jacobienne

--- Newton_Rhapson.py
+++ Newton_Rhapson.py
@@ -4,22 +4,7 @@
 
 from scipy import misc
 import numpy as np
-import sympy
 import matplotlib.pyplot as plt
-
-#Fonction de calcul de la jacobienne
-def compute_jacobian(vectorial, x):
-    h = 1.0e-4
-    n = len(x)
-    jacobian = np.zeros((n, n))
-    f0 = vectorial(x)
-    for i in range(n):
-        temp = x[i]
-        x[i] = temp + h
-        f1 = vectorial(x)
-        x[i] = temp
-        jacobian[:,i] = (f1 - f0)/h
-    return jacobian, f0
 
 ################### Newton-Raphson ######################
 
@@ -57,10 +42,6 @@ def Newton_Raphson_Back(f, J, U0, N, epsilon):
     return U
 
 ###### Test part #######
-
-x1, x2, x3 = sympy.symbols('x1 x2 x3', Real=True)
-
-compute_jacobian()
 
 def f(x):
     return np.array([x[0]*x[0]-2, x[1]*x[1]-5])
