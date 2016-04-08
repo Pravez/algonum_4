e66ca0bd668eff7e417fc65289b45666bcc17cfa
Ludovic42 <pflegerludovic@gmail.com>
2016-04-08 12:30:13 +0200

    Update Lagrange.py

--- Lagrange.py
+++ Lagrange.py
@@ -26,7 +26,46 @@ def f(x, y):
 
     return (fx,fy)
 
-def J(
+def f(U):
+    return np.array([f1(U),f2(U)])
 
+#Plus comprehensible
+def forces_sum(U):
+    f1=gravitational_force(0,0,x,y,1)
+    f2=gravitational_force(1,0,x,y,0.01)
+    m1=1
+    m2=0.01
+    f3=centrifugal_force((m1*0+m2*1)/(m1+m2),(m1*0+m2*0)/(m1+m2),1)
+    return f1+f2+f3
 
-def equilibrium_points(Ux0 ,Uy0):
+#Derivee de f selon x
+def deriv_x(f,x0,y0,k,U):
+    h=10**-10
+    T=np.array([h,0])
+    return ((f(U+T,x0,y0,k) - f(U-T,x0,y0,k))/(2*h))
+
+def deriv_y(f,x0,y0,k,U):
+    h=10**-10
+    T=np.array([0,h])
+    return ((f(U+T,x0,y0,k) - f(U-T,x0,y0,k))/(2*h))
+
+def gravit_jac(U,x0,y0,k):
+    J=np.array([deriv_x(gravitational_force,x0,y0,k,U),deriv_y(gravitational_for
+ce,x0,y0,k,U)])
+    return J
+
+def centrif_jac(U,x0,y0,k):
+    J=np.array([deriv_x(centrifugal_force,x0,y0,k,U),deriv_y(centrifugal_force,x
+0,y0,k,U)])
+    return J
+
+def Jacobian_matrix(U):
+    f1=gravit_jac(U,0,0,1)
+    f2=gravit_jac(U,1,0,0.01)
+    m1=1
+    m2=0.01
+    x0=(m1*0+m2*1)/(m1+m2)
+    y0=(m1*0+m2*0)/(m1+m2)
+    f3=centrif_jac(U,x0,y0,1)
+    return f1+f2+f3
+
