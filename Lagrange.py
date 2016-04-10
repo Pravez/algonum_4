2d909fec22f2a331985b5705fb3fb85a0b1ba583
Ludovic42 <pflegerludovic@gmail.com>
2016-04-10 11:30:52 +0200

    Update Lagrange.py

--- Lagrange.py
+++ Lagrange.py
@@ -1,7 +1,5 @@
 import numpy as np
-import numdifftools as nd
 from Newton_Raphson.py import *
-
 
 def elastic_force(dx,k):
     return -dx*k
@@ -13,23 +11,9 @@ def gravitational_force(x0 ,y0, x, y, k):
     return (-k*(x-x0)/((x-x0)**2 + (y-y0)**2)**(3/2),
             -k*(y-y0)/((x-x0)**2 + (y-y0)**2)**(3/2))
 
-def f(x, y):
-    b=(0.01/1.01, 0)
-
-    fx= gravitational_force(0, 0, x, y, 1)[0] +
-    gravitational_force(1, 0, x, y, 0.01)[0] +
-    centrifugal_force(b[0], b[1], x, y, 1)[0]
-
-    fy= gravitational_force(0, 0, x, y, 1)[1] +
-    gravitational_force(1, 0, x, y, 0.01)[1] +
-    centrifugal_force(b[0], b[1], x, y, 1)[1]
-
-    return (fx,fy)
-
 def f(U):
     return np.array([f1(U),f2(U)])
 
-#Plus comprehensible
 def forces_sum(U):
     f1=gravitational_force(0,0,x,y,1)
     f2=gravitational_force(1,0,x,y,0.01)
@@ -38,7 +22,6 @@ def forces_sum(U):
     f3=centrifugal_force((m1*0+m2*1)/(m1+m2),(m1*0+m2*0)/(m1+m2),1)
     return f1+f2+f3
 
-#Derivee de f selon x
 def deriv_x(f,x0,y0,k,U):
     h=10**-10
     T=np.array([h,0])
